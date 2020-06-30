ac53782220b053fb2fca8c29349588cfbaf78257
vanhelmont <54476474+vanhelmont@users.noreply.github.com>
2020-06-30 09:00:52 -0300

    Return category of trophic state and formatting
    Returns the trophic state corresponding to each variable. Modified
    formating so TSI values are integer. Also add some heading.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# tsi
-Determines Trophic State Index (TSI) for a given water body
+# TSI
+Determines Trophic State Index (TSI) for a lake sample.
+
+## Features
+Currently, the user enters the value of three variables needed for calculate the TSI values of each.
--- tsi_calc.py
+++ tsi_calc.py
@@ -1,20 +1,40 @@
 from math import log
 
 def tsi_secchi(secchi):
-	tsi_secchi = 10 * (2.46 + (3.67-1.57*log(secchi))/log(2.5)) 
-	print('The TSI-Secchi corresponding to a Secchi value of ' + str(secchi) + 'm is ' + str(tsi_secchi) + '.')
+	tsi_secchi = 10 * (2.46 + (3.67-1.57*log(secchi))/log(2.5))
+	tsi_secchi = int(round(tsi_secchi, 0)) 
+	ts_secchi = check_tsi(tsi_secchi)
+	print('The TSI-Secchi corresponding to a Secchi value of ' + str(secchi) + 'm is ' + str(tsi_secchi) + ' - Categorized: ' + ts_secchi)
 
 
 def tsi_chla(chla):
 	tsi_chla = 10 * (2.46 + log(chla)/log(2.5))
-	print('The TSI-Chl corresponding to a Secchi value of ' + str(chla) + 'm is ' + str(tsi_chla) + '.')
+	tsi_chla = int(round(tsi_chla, 0))
+	ts_chla = check_tsi(tsi_chla)
+	print('The TSI-Chl corresponding to a Secchi value of ' + str(chla) + 'm is ' + str(tsi_chla) + ' - Categorized: ' + ts_chla)
 
 def tsi_tp(tp):
 	tsi_tp = 10 * (2.46 + (6.68-1.15*log(tp))/log(2.5))
-	print('The TSI-Secchi corresponding to a TP value of ' + str(tp) + 'm is ' + str(tsi_tp) + '.')
+	tsi_tp = int(round(tsi_tp, 0))
+	ts_tp = check_tsi(tsi_tp)
+	print('The TSI-Secchi corresponding to a TP value of ' + str(tp) + 'm is ' + str(tsi_tp) + ' - Categorized: ' + ts_tp)
 
-# def check_tsi(tsi_value):
+def check_tsi(tsi_value):
+	if tsi_value >= 0 and tsi_value <= 100:
+		if tsi_value < 30:
+			return 'Oligotrphic'
+		elif tsi_value < 60:
+			return 'Mesotrophic'
+		elif tsi_value < 90:
+			return 'Eutrophic'
+		else:
+			return 'Hipereutrophic'
+	else:
+		return 'ERROR'
 
+# START
+version = '0.1'
+print('TSI-CALCULATOR - v{0}'.format(version))
 
 # User input
 user_secchi = float(input('Input Secchi depth (in meters): '))
@@ -26,7 +46,7 @@ print('')
 tsi_secchi(user_secchi)
 tsi_chla(user_chla)
 tsi_tp(user_tp)
+print('')
 
 # Press any key to exit
-print('')
 input('Press any key to exit.')
